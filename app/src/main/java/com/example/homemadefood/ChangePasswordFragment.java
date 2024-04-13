fb0a45574322d36674b77d769df22a357ad380f0
Quang Phu Ly <quangphuly150402@gmail.com>
2024-04-13 19:40:22 -0400

    Change Drink Menu and Update password

--- app/src/main/java/com/example/homemadefood/ChangePasswordFragment.java
+++ app/src/main/java/com/example/homemadefood/ChangePasswordFragment.java
@@ -1,5 +1,8 @@
 package com.example.homemadefood;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,9 +12,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,7 +42,10 @@ public class ChangePasswordFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    TextInputEditText oldPass, newPass, confirmNewPass;
+    TextInputLayout oldPass, newPass, confirmNewPass;
+    FirebaseFirestore db;
+    private static final String SHARED_PREF_NAME = "homemadefood_shared_pref";
+    private static final String KEY_USERNAME = "username";
 
     public ChangePasswordFragment() {
         // Required empty public constructor
@@ -74,5 +91,86 @@ public class ChangePasswordFragment extends Fragment {
         oldPass = view.findViewById(R.id.oldPassword);
         newPass = view.findViewById(R.id.new_password);
         confirmNewPass = view.findViewById(R.id.confirm_new_password);
+
+        Button changePass = view.findViewById(R.id.changePassword);
+
+        changePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatePassword();
+            }
+        });
+    }
+
+    private void updatePassword() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        String username = sharedPreferences.getString(KEY_USERNAME, "");
+
+        db = FirebaseFirestore.getInstance();
+        CollectionReference usersCollection = db.collection("users");
+        DocumentReference docRef = usersCollection.document(username);
+
+        String oldPassword = oldPass.getEditText().getText().toString();
+        String newPassword = newPass.getEditText().getText().toString();
+        String confirmNewPassword = confirmNewPass.getEditText().getText().toString();
+
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String pass = documentSnapshot.getString("passwd");
+                if (!oldPassword.equals(pass)) {
+                    Toast.makeText(getContext(), "Incorrect Old Password", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (!validatePassword()) {
+                        Toast.makeText(getContext(), "Please enter all fields", Toast.LENGTH_SHORT).show();
+                    } else if (!newPassword.equals(confirmNewPassword)) {
+                        Toast.makeText(getContext(), "Confirm Password need to be identical with your new password", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Map<String, Object> updates = new HashMap<>();
+                        updates.put("passwd", newPassword);
+                        docRef.update(updates).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(getContext(), "Change Password Successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getContext(), "Fail to update password", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Fail to change Password", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private Boolean validatePassword() {
+        String val = newPass.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+//                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+        if (val.isEmpty()) {
+            newPass.setError("Field cannot be empty");
+            return false;
+        } else if (!val.matches(passwordVal)) {
+            newPass.setError("Password is too weak");
+            return false;
+        } else {
+            newPass.setError(null);
+            newPass.setErrorEnabled(false);
+            return true;
+        }
     }
 }
